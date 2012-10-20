c57630563376d5cff6f3225395d99a49bae546c8
Steven <skravik@gmail.com>
2012-10-20 06:32:46 -0500

    Detule: bcm_bt_lpm in 3.2

--- include/linux/platform_data/msm_serial_hs.h
+++ include/linux/platform_data/msm_serial_hs.h
@@ -22,7 +22,9 @@
  * and must call request_clock_on() before any further uart data can be
  * received. */
 extern void msm_hs_request_clock_off(struct uart_port *uport);
+extern void msm_hs_request_clock_off_locked(struct uart_port *uport);
 extern void msm_hs_request_clock_on(struct uart_port *uport);
+extern void msm_hs_request_clock_on_locked(struct uart_port *uport);
 
 /**
  * struct msm_serial_hs_platform_data
