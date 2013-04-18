9a59566ca09a31fa9d72ba19d870da916563c68e
Steven <skravik@gmail.com>
2013-04-17 20:27:36 -0500

    mddi: fix section mismatches

--- drivers/video/msm/mddi.c
+++ drivers/video/msm/mddi.c
@@ -487,7 +487,7 @@ static void mddi_resume(struct msm_mddi_client_data *cdata)
 	printk("mddi_resumed\n");
 }
 
-static int __init mddi_get_client_caps(struct mddi_info *mddi)
+static int __devinit mddi_get_client_caps(struct mddi_info *mddi)
 {
 	int i, j;
 
@@ -719,7 +719,7 @@ uint32_t mddi_remote_read(struct msm_mddi_client_data *cdata, uint32_t reg)
 
 static struct mddi_info mddi_info[2];
 
-static int __init mddi_clk_setup(struct platform_device *pdev,
+static int __devinit mddi_clk_setup(struct platform_device *pdev,
 				 struct mddi_info *mddi,
 				 unsigned long clk_rate)
 {
@@ -745,7 +745,7 @@ fail:
 	return ret;
 }
 
-static int __init mddi_rev_data_setup(struct mddi_info *mddi)
+static int __devinit mddi_rev_data_setup(struct mddi_info *mddi)
 {
 	void *dma;
 	dma_addr_t dma_addr;
@@ -765,7 +765,7 @@ static int __init mddi_rev_data_setup(struct mddi_info *mddi)
 	return 0;
 }
 
-static int mddi_probe(struct platform_device *pdev)
+static int __devinit mddi_probe(struct platform_device *pdev)
 {
 	struct msm_mddi_platform_data *pdata = pdev->dev.platform_data;
 	struct mddi_info *mddi = &mddi_info[pdev->id];
